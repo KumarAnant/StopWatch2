53b2049791887d7cbfc65af3d1a992b144a44eec
KumarAnant <KumarAnant@users.noreply.github.com>
2018-06-11 00:33:16 -0400

    Basic Code

--- app/src/main/java/pro/anant/android/stopwatch2/StopwatchActivity.java
+++ app/src/main/java/pro/anant/android/stopwatch2/StopwatchActivity.java
@@ -2,12 +2,29 @@ package pro.anant.android.stopwatch2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
     }
+    public void onClickStart(View view){
+        running = true;
+    }
+
+    public void onClickStop(View view){
+        running = false;
+    }
+
+    public void onClickReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
 }
