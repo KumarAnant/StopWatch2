eade9be04352502be6cf4bf9416172d485af3b49
KumarAnant <KumarAnant@users.noreply.github.com>
2018-06-11 00:12:17 -0400

    Initial Commmit

---
+++ app/src/main/java/pro/anant/android/stopwatch2/StopwatchActivity.java
@@ -0,0 +1,13 @@
+package pro.anant.android.stopwatch2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StopwatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+    }
+}
