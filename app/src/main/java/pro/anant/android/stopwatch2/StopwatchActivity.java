89e54a809b08d02e4d44f0f91cffd36249e60278
Anant Kumar <anantkumar11@gmail.com>
2018-06-11 14:34:01 -0400

    Working stopwatch in a given layout

--- app/src/main/java/pro/anant/android/stopwatch2/StopwatchActivity.java
+++ app/src/main/java/pro/anant/android/stopwatch2/StopwatchActivity.java
@@ -2,7 +2,11 @@ package pro.anant.android.stopwatch2;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class StopwatchActivity extends Activity {
 
@@ -13,6 +17,7 @@ public class StopwatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
     }
     public void onClickStart(View view){
         running = true;
@@ -25,6 +30,25 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view){
         running = false;
         seconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int sec = seconds % 60;
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, sec);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 
 }
